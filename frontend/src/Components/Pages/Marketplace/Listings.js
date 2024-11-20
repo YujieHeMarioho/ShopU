c9b401fd886b241b6e274a8b445c1bcf0dc33df2
kevintann11 <kevintann4@gmail.com>
2024-11-19 22:48:17 -0700

    - Adjusted CSS - Adjusted Dynamic drop down logic -Comments misc

--- frontend/src/Components/Pages/Marketplace/Listings.js
+++ frontend/src/Components/Pages/Marketplace/Listings.js
@@ -1,33 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Modal, Button, Dropdown, DropdownButton, DropdownItem } from 'react-bootstrap';
 import './Listings.css'; // Import the CSS file
 
 function ListingModal({ show, onHide, listing }) {
+ const [dropDownTitle, setDropDownTitle] = useState('Select an Option')
+
   if (!listing) return null; // If no listing data, render nothing
 
+  const handleDropDownClick = (option) => {
+    setDropDownTitle(option);
+  };
+
   return (
-    <Modal show={show} onHide={onHide} size="xl" centered>
-    <Modal.Header closeButton>
-      <Modal.Title className="modal-title">{listing.title}</Modal.Title>
-    </Modal.Header>
-    <Modal.Body className="modal-body"> {/* Image on the left */} <img src={listing.image} alt={listing.title} className="img-fluid mb-3" /> {/* Content on the right */} <div className="content">
-        <h5 className="card-price">Price: ${listing.price}</h5>
-        <div className="product-options">
-          <label htmlFor="product-options-dropdown" className="card-text"> Product Options </label>
-          <DropdownButton id="product-options-dropdown" title='Pickup From Seller' className="options-dropdown">
-            <Dropdown.Item> Drop Off Location </Dropdown.Item>
-            <Dropdown.Item> Shipped </Dropdown.Item>
-          </DropdownButton>
-        </div>
-        <Button variant="secondary" className="mb-2"> Offer Seller! </Button>
-        <Button variant="secondary"> Message Seller! </Button>
-        <p className="card-text">{listing.description}</p>
-      </div>
-    </Modal.Body>
-    <Modal.Footer>
-      <h5 class='card-text'> add future similar listings here </h5>
-    </Modal.Footer>
-  </Modal>
+    <Modal show={show} onHide={onHide} dialogClassName='modal' centered>
+     <Modal.Header closeButton>
+       <Modal.Title className="card-title">{listing.title}</Modal.Title>
+     </Modal.Header>
+     <Modal.Body className="modal-body"> 
+    <img src={listing.image} alt={listing.title} className="img" /> 
+     <div className="content">
+         <h5 className="card-price">${listing.price}</h5>
+         <div className="product-options">
+           <label htmlFor="product-options-dropdown" className="card-text"> Product Options </label>
+           <Dropdown>
+             <Dropdown.Toggle id="product-options-dropdown" variant="outline-light" className="options-dropdown"> {dropDownTitle} </Dropdown.Toggle>
+             <Dropdown.Menu>
+               <Dropdown.Item as="button" onClick={()=> handleDropDownClick('Drop Off Location')}>Drop Off Location</Dropdown.Item>
+               <Dropdown.Item as="button" onClick={()=> handleDropDownClick('Pickup From Seller')}>Pickup From Seller</Dropdown.Item>
+               <Dropdown.Item as="button" onClick={()=> handleDropDownClick('Shipped')}>Shipped</Dropdown.Item>
+             </Dropdown.Menu>
+           </Dropdown>
+         </div>
+         <Button variant="dark" className="mb-2"> Offer Seller! </Button>
+         <Button variant="dark"> Message Seller! </Button>
+         <p className="card-text">{listing.description}</p>
+       </div>
+     </Modal.Body>
+     <Modal.Footer>
+       <h5 class='card-text'> add future similar listings here </h5>
+     </Modal.Footer>
+   </Modal>
   );
 }
 
