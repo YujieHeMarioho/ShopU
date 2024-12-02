395b352ea19483e4f4c5deb4a606613b47c09f94
Alice <u1275258@utah.edu>
2024-12-02 01:36:59 -0700

    add new tables for communities

--- awsdb.session.sql
+++ awsdb.session.sql
@@ -1,28 +1,11 @@
+CREATE TABLE communities (
+  community_id BIGINT,
+  name VARCHAR(255),
+  description VARCHAR(255)
+);
 
-ALTER TABLE users
-ADD COLUMN auth0_id VARCHAR(255) UNIQUE NOT NULL;
-
-
-ALTER TABLE users
-ADD COLUMN is_admin BOOLEAN DEFAULT FALSE;
-
-
-
-INSERT INTO users (
-    user_id,
-    edu_email,
-    profile_picture,
-    phone_number,
-    first_name,
-    last_name,
-    create_date
-  )
-VALUES (
-    user_id:integer,
-    'edu_email:character varying',
-    'profile_picture:character varying',
-    'phone_number:character varying',
-    'first_name:character varying',
-    'last_name:character varying',
-    'create_date:date'
-  );
+CREATE TABLE community_members (
+  community_id BIGINT,
+  user_id INT,
+  joined_at TIMESTAMP
+);
