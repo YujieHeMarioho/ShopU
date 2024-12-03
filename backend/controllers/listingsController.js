5ab22d06b0950cc011f378dcee45f1c8d4016d78
kevintann11 <kevintann4@gmail.com>
2024-12-02 17:23:52 -0700

    - Fixed Bug where rendering wouldn't poll (this was due to filtering)

--- backend/controllers/listingsController.js
+++ backend/controllers/listingsController.js
@@ -5,8 +5,7 @@ export const getAllListings = async (req, res) => {
     const query = 'SELECT * FROM public.listings';  
     try {
         const result = await pool.query(query);  
-        res.status(200).json(result.rows);
-        res.json(result.rows);  
+        res.status(200).json(result.rows); 
     } catch (err) {
         console.error('Error running query:', err);  
         res.status(500).json({ error: 'Database error' });  
