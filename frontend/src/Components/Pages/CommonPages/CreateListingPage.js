33a7d8a06c418fb20ba196211a0b650c0519c360
kevintann11 <kevintann4@gmail.com>
2025-01-15 16:19:58 -0700

    - New dependencies - Multiple Image upload - Image carousel - S3 Bucket Initialization - Backend API for uploads - Env properties

--- frontend/src/Components/Pages/CommonPages/CreateListingPage.js
+++ frontend/src/Components/Pages/CommonPages/CreateListingPage.js
@@ -122,6 +122,11 @@ const CreateListingPage = () => {
       // Appending files to formData
       images.forEach((image) => formData.append('images', image));
       
+      // Log the contents of formData by iterating through its entries
+      formData.forEach((value, key) => {
+        console.log(key, value);  // key will be 'images', value will be the file
+      });
+      
       const token = await getAccessTokenSilently();
       const response = await fetch('http://localhost:8080/api/listings/uploadImages', {
         method: 'POST',
@@ -136,8 +141,7 @@ const CreateListingPage = () => {
       }
   
       const data = await response.json();
-
-      return data.files; // Assuming the backend returns an array of URLs
+      return data.fileUrls; // Assuming the backend returns an array of URLs
     } catch (error) {
       console.error('Error uploading images:', error);
       return [];
