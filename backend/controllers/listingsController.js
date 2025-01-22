7dc21081848ac73375fc1988f00df734966b1571
kevintann11 <kevintann4@gmail.com>
2025-01-21 22:02:01 -0700

    - Created new images table - Added some new possible endpoints - Image data successfully saved to db

--- backend/controllers/listingsController.js
+++ backend/controllers/listingsController.js
@@ -2,7 +2,7 @@ import pool from '../pool.js';
 import { jwtDecode } from "jwt-decode";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import { v4 as uuidv4 } from "uuid"; 
-import dotenv from 'dotenv';
+import dotenv, { parse } from 'dotenv';
 
 dotenv.config();
 
@@ -90,10 +90,11 @@ export const getFavoritedListings = async (req, res) => {
 //Endpoint for create a listing
 export const createListing = async (req, res) => {
     try {
-        const { title, description, category, type, rating, price, condition} = req.body;
-        // Retrieve uploaded image path from multer or use a default placeholder if none provided
+        const { title, description, category, type, rating, price, condition, images} = req.body;
+        const parsedImages  =  images ? JSON.parse(images) : [];
+        console.log(parsedImages.length);
+                // Retrieve uploaded image path from multer or use a default placeholder if none provided
         const imagePath = req.file ? req.file.path : `${process.env.REACT_APP_AUTH0_REDIRECT_URI}/ShopULogo.png`;
-
         let userId;
         const token = req.headers.authorization && req.headers.authorization.split(' ')[1];
         try {
@@ -104,26 +105,74 @@ export const createListing = async (req, res) => {
             return res.status(401).json({ message: 'Invalid token' });
         }
       
-        //database call to get category id
-
-        const query = `
+        //database call to create listing in listing table
+        const listingQuery = `
             INSERT INTO public.listings (title, description, category_id, item_type, star_rating, price, image_url, condition, date_posted, user_id)
             VALUES ($1, $2, 1, $3, $4, $5, $6, $7, $8, $9)
             RETURNING *;
         `;
-        const values = [title, description, type, rating || 0, price, imagePath, condition, new Date().toISOString(), userId];
+        const listingValues = [title, description, type, rating || 0, price, imagePath, condition, new Date().toISOString(), userId];
+
         
-        // Execute query
-        const result = await pool.query(query, values);
-        res.status(201).json(result.rows[0]);
+        // Execute listings table query
+        const listingResult = await pool.query(listingQuery, listingValues);
+
+        for (const imageKey of parsedImages) {
+          const imageQuery = `
+              INSERT INTO public.listing_images (listing_id, file_key)
+              VALUES ($1, $2)
+          `;
+          const imageValues = [listingResult.rows[0].listing_id, imageKey];
+
+          // Execute image insertion query
+          await pool.query(imageQuery, imageValues);
+      }
+
+        res.status(201).json(listingResult.rows[0]);
     } catch (err) {
         console.error('Error creating listing:', err);
         res.status(500).json({ error: 'Database error' });
     }
 };
 
+//Endpoint for create a listing
+export const createServiceListing = async (req, res) => {
+  try {
+      const { title, description, category, type, rating, price, condition, images} = req.body;
+      
+      let userId;
+      const token = req.headers.authorization && req.headers.authorization.split(' ')[1];
+      try {
+          const decodedToken = jwtDecode(token); // Decode the token
+          userId = decodedToken.sub;
+      } catch (err) {
+          console.error('Error decoding token:', err);
+          return res.status(401).json({ message: 'Invalid token' });
+      }
+    
+      //database call to create listing in listing table
+      const listingQuery = `
+          INSERT INTO public.listings (title, description, category_id, item_type, star_rating, price, condition, date_posted, user_id)
+          VALUES ($1, $2, 1, $3, $4, $5, $1, $6, $7, $8)
+          RETURNING *;
+      `;
+      const listingValues = [title, description, type, rating || 0, price, condition, new Date().toISOString(), userId];
+      
+      // Execute listings table query
+      const listingResult = await pool.query(listingQuery, listingValues);
+
+      const imageQuery = `INSERT INTO public.listings`;
+
+      const imageValues = [];
+      
+      res.status(201).json(listingQuery.rows[0]);
+  } catch (err) {
+      console.error('Error creating listing:', err);
+      res.status(500).json({ error: 'Database error' });
+  }
+};
+
 export const uploadImages = async (req, res) => {
-  console.log('Reached backend api, this is the amount of files: ' + req.files.length);
   try {
     if (!req.files || req.files.length === 0) {
       return res.status(400).json({ message: "No files uploaded" });
@@ -132,11 +181,10 @@ export const uploadImages = async (req, res) => {
     const uploadedFiles = [];
 
     for (const file of req.files) {
-      console.log(file);
-      const fileKey = `${uuidv4()}-${file.originalname}`;
+      const fileName = `${uuidv4()}-${file.originalname}`;
       const params = {
         Bucket: bucketName,
-        Key: fileKey,
+        Key: fileName,
         Body: file.buffer,
         ContentType: file.mimetype,
       };
@@ -145,8 +193,8 @@ export const uploadImages = async (req, res) => {
       await s3.send(new PutObjectCommand(params));
 
       // Store the S3 URL in the response
-      const fileUrl = `https://${process.env.BUCKET_NAME}.s3.${process.env.BUCKET_REGION}.amazonaws.com/${fileKey}`;
-      uploadedFiles.push(fileUrl);
+      const fileUrl = `https://${process.env.BUCKET_NAME}.s3.${process.env.BUCKET_REGION}.amazonaws.com/${fileName}`;
+      uploadedFiles.push(fileName);
     }
 
     res.status(200).json({
@@ -159,4 +207,4 @@ export const uploadImages = async (req, res) => {
   }
 };
 
-export default {createListing, getFavoritedListings, getAllListings, uploadImages};
+export default {createListing, getFavoritedListings, getAllListings, uploadImages, createServiceListing};
