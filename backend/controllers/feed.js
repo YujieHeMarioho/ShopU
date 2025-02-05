c136757e82bb0ca5195099fb7d49175bbb752e39
kevintann11 <kevintann4@gmail.com>
2025-02-05 12:29:58 -0700

    - Setup for images for feed

--- backend/controllers/feed.js
+++ backend/controllers/feed.js
@@ -1,11 +1,11 @@
-import { s3, pool } from '../pool.js';
+import { s3, pool, buckets } from '../pool.js';
 import { jwtDecode } from 'jwt-decode';
 import { PutObjectCommand, GetObjectCommand } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { v4 as uuidv4 } from 'uuid';
 import sharp from 'sharp';
 
-const bucketName = process.env.BUCKET_NAME_FEED
+const bucketName = buckets.feed;
 
 // Helper function to extract user_id from the token
 const extractUserIdFromToken = (req) => {
