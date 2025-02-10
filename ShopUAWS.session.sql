97a8624c41360a6c8c67ede84f72ceed913b53c7
kevintann11 <kevintann4@gmail.com>
2025-02-10 16:56:26 -0700

    ignore changes to the sql query file

--- ShopUAWS.session.sql
+++ ShopUAWS.session.sql
@@ -1,1 +1,24 @@
-select * from communities;
+        SELECT
+          l.listing_id,
+          l.user_id,
+          l.title,
+          l.description,
+          c.name AS category,  -- Get the category name
+          l.item_type,
+          l.star_rating,
+          l.price,
+          ARRAY_AGG(li.file_key) AS file_keys 
+        FROM
+          listings l
+        JOIN
+          categories c ON l.category_id = c.category_id
+        INNER JOIN
+          listing_images li ON l.listing_id = li.listing_id
+        GROUP BY
+          l.listing_id, 
+          l.title, 
+          l.description, 
+          c.name, 
+          l.item_type, 
+          l.star_rating, 
+          l.price;
