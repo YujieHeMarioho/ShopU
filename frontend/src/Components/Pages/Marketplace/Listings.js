956df815aac0cd12640874bcb2b17fdfd061ef1b
kevintann11 <kevintann4@gmail.com>
2025-02-10 16:13:53 -0700

    - CSS and HTML for editing buttons

--- frontend/src/Components/Pages/Marketplace/Listings.js
+++ frontend/src/Components/Pages/Marketplace/Listings.js
@@ -13,57 +13,59 @@ function ListingModal({ show, onHide, listing }) {
 
   return (
     <Modal show={show} onHide={onHide} dialogClassName='modal' centered>
-      <Modal.Header closeButton>
-          <Modal.Title className='card-title'>{listing.title}</Modal.Title>
-          <div className='edit-buttons'>
-          <Button variant="secondary">Edit</Button>
-          <Button variant="danger">Delete</Button>
-          </div>
-      </Modal.Header>
-      <Modal.Body className='modal-body'>
-          <div className='carousel-container'>
-              {listing.image.length > 1 ? (
-              <Carousel interval={null} slide={false}>
-                {listing.image.map((img, index) => (
-                  <Carousel.Item key={index}>
-                    <img
-                      src={img}
-                      alt={`Slide ${index}`}
-                      className='img'
-                    />
-                  </Carousel.Item>
-                ))}
-              </Carousel>
-            ) : (
-              <img
-                src={listing.image[0]}
-                alt={listing.title}
+    <Modal.Header closeButton>
+       <Modal.Title className='card-title'>{listing.title}</Modal.Title>
+    </Modal.Header>
+    <Modal.Body className='modal-body'>
+       <div className='carousel-container'>
+          {listing.image.length > 1 ? (
+          <Carousel interval={null} slide={false}>
+             {listing.image.map((img, index) => (
+             <Carousel.Item key={index}>
+                <img
+                src={img}
+                alt={`Slide ${index}`}
                 className='img'
-              />
-            )}
+                />
+             </Carousel.Item>
+             ))}
+          </Carousel>
+          ) : (
+          <img
+             src={listing.image[0]}
+             alt={listing.title}
+             className='img'
+             />
+          )}
+       </div>
+       <div className='content'>
+          <div className='price-and-buttons'>
+             <h5 className='card-price'>${listing.price}</h5>
+             <div className='edit-buttons'>
+                <Button variant="secondary">Edit</Button>
+                <Button variant="danger">Delete</Button>
+             </div>
           </div>
-          <div className='content'>
-            <h5 className='card-price'>${listing.price}</h5>
-            <div className='product-options'>
-                <label htmlFor='product-options-dropdown' className='card-text'> Product Options </label>
-                <Dropdown>
-                  <Dropdown.Toggle id='product-options-dropdown' variant='outline-light' className='dropdown-text'> {dropDownTitle} </Dropdown.Toggle>
-                  <Dropdown.Menu>
-                      <Dropdown.Item as='button' onClick={()=> handleDropDownClick('Drop Off Location')}>Drop Off Location</Dropdown.Item>
-                      <Dropdown.Item as='button' onClick={()=> handleDropDownClick('Pickup From Seller')}>Pickup From Seller</Dropdown.Item>
-                      <Dropdown.Item as='button' onClick={()=> handleDropDownClick('Shipped')}>Shipped</Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
-            </div>
-            <Button variant='dark' className='mb-2'> Offer Seller! </Button>
-            <Button variant='dark'> Message Seller! </Button>
-            <p className='card-text'>{listing.description}</p>
+          <div className='product-options'>
+             <label htmlFor='product-options-dropdown' className='card-text'> Product Options </label>
+             <Dropdown>
+                <Dropdown.Toggle id='product-options-dropdown' variant='outline-light' className='dropdown-text'> {dropDownTitle} </Dropdown.Toggle>
+                <Dropdown.Menu>
+                   <Dropdown.Item as='button' onClick={()=> handleDropDownClick('Drop Off Location')}>Drop Off Location</Dropdown.Item>
+                   <Dropdown.Item as='button' onClick={()=> handleDropDownClick('Pickup From Seller')}>Pickup From Seller</Dropdown.Item>
+                   <Dropdown.Item as='button' onClick={()=> handleDropDownClick('Shipped')}>Shipped</Dropdown.Item>
+                </Dropdown.Menu>
+             </Dropdown>
           </div>
-      </Modal.Body>
-      <Modal.Footer>
-          <h5 class='card-text'> add future similar listings here </h5>
-      </Modal.Footer>
-    </Modal>
+          <Button variant='dark' className='mb-2'> Offer Seller! </Button>
+          <Button variant='dark'> Message Seller! </Button>
+          <p className='card-text'>{listing.description}</p>
+       </div>
+    </Modal.Body>
+    <Modal.Footer>
+       <h5 class='card-text'> add future similar listings here </h5>
+    </Modal.Footer>
+ </Modal>
   );
 }
 
