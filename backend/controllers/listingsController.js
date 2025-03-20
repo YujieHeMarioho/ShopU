0a2e0559559d9f8c9917aa078769333ef3f8b34e
kevintann11 <kevintann4@gmail.com>
2025-03-20 17:59:42 -0600

    - Create Service listings - Service listings now display listings where product options were once.

--- backend/controllers/listingsController.js
+++ backend/controllers/listingsController.js
@@ -37,24 +37,30 @@ export const getAllListings = async (req, res) => {
       l.user_id,
       l.title,
       l.description,
-      c.name AS category,  -- Get the category name
+      c.name AS category,
       l.item_type,
       l.price,
       l.location,
-      ARRAY_AGG(li.file_key) AS file_keys,
-      u.name AS author,     
-      u.profile_image AS profile   
-    FROM
-      listings l
-    JOIN
-      categories c ON l.category_id = c.category_id
-    JOIN
-      users u ON l.user_id = u.user_id
-    INNER JOIN
-      listing_images li ON l.listing_id = li.listing_id
+      ARRAY_AGG(DISTINCT li.file_key) AS file_keys,
+      u.name AS author,
+      u.profile_image AS profile,
+      COALESCE(
+        JSON_AGG(
+          DISTINCT JSONB_BUILD_OBJECT(
+            'service_id', ls.service_id,
+            'service_name', ls.service_name,
+            'service_price', ls.service_price
+          )
+        ) FILTER (WHERE ls.service_id IS NOT NULL), '[]'::JSON
+      ) AS services
+    FROM listings l
+    JOIN categories c ON l.category_id = c.category_id
+    JOIN users u ON l.user_id = u.user_id
+    LEFT JOIN listing_images li ON l.listing_id = li.listing_id
+    LEFT JOIN listing_services ls ON l.listing_id = ls.listing_id
     GROUP BY
       l.listing_id, 
-      l.user_id,          -- Group by seller's ID
+      l.user_id,          
       l.title, 
       l.description, 
       c.name, 
@@ -62,9 +68,9 @@ export const getAllListings = async (req, res) => {
       l.price,
       l.location,
       u.name,               
-      u.profile_image;      
-      `;
-
+      u.profile_image;
+  `;
+  
   try {
     const result = await pool.query(query);
     // Loop through each listing and generate signed URLs
